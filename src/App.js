7ca12f76223c47a27d918b7563a0c6aa9dd3889d
Kevin Tang <kevintang@Kevins-MacBook-Pro.local>
2022-02-23 00:09:33 -0500

    added posts and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import Post from './Post'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+
+
+      <div className="app-header">
+        <img
+          className="app-header-image"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+          alt=""
+        />
+      </div>
+
+      {/* Header */}
+
+      <Post />
+      {/* Posts */}
+
+      {/* Posts */}
+
+      <h1> Hello Clever Programmers </h1>
     </div>
   );
 }
