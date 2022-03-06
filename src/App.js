1d528ead95f19f85de5892fe3553881b08970359
kevvtang <kt27839@uga.edu>
2022-03-06 00:15:27 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Post'
+import { db } from './firebase';
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection('posts').onSnapshot(snapshot => {
+      setPosts(snapshot.docs.map(doc => doc.data()))
+    })
+  }, []);
+
   return (
     <div className="app">
-
-
       <div className="app-header">
         <img
           className="app-header-image"
@@ -17,12 +24,12 @@ function App() {
 
       {/* Header */}
 
-      <Post />
-      {/* Posts */}
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}> </Post>
+        ))
+      }
 
-      {/* Posts */}
-
-      <h1> Hello Clever Programmers </h1>
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import './Post.css'
+import { Avatar } from '@material-ui/core'
 
-function Post() {
+function Post({username, caption, imageUrl}) {
     return (
-        <div>
+        <div className='post'>
+
+            <div className="post_header">
+                <Avatar className = "post_avatar"> 
+                    alt = 'Kevin'
+                    src="/static/images/avatar/1.jpg"
+                </Avatar>
+                <h3> {username} </h3>
+            </div>
+
             {/* header -> avatar + username */}
             
-            <img className="post-image" src="https://m.media-amazon.com/images/I/51W6duELHdL._AC_.jpg" />
+            <img className="post_image" src={imageUrl} alt=""/>
             {/* image */}
             
-            <h4> Username: caption </h4>
+            <h4 className='post_text'> <strong> {username} </strong> {caption} </h4>
             {/* username + caption */}
         </div>
     )
